ace289099827b1270c5a4fa05cb3369df47a197e
Fabian Caicedo <jefedesarrollo@talentum.edu.co>
2022-11-10 15:32:04 -0500

    App docker compose updater

---
+++ README.md
@@ -0,0 +1,28 @@
+# docker-updater
+Bash application that allows updating applications based on docker compose.
+The application checks if there is a new release in a given public repository, if there is a new latest version, the application displays the new version and if there is an error in the download, the previous version is restored.
+For the application to work correctly you must run the first time without errors so that it can save the previous version if a new version is created.
+
+## Prerequisitos
+You need to have the following packages installed on Linux:
+
+* Install docker and docker compose ()
+* nohup
+* jq
+* curl
+* wget
+* unzip
+* sh files must have at least the following permissions:
+```
+    chmod +x file.sh
+```
+
+## How to use
+You can use the application in 2 ways:
+1. Sequentially to update several but until you update an application, it does not continue with the next one.
+For this you must use the 2 sh that are inside the repository (apps-updater.sh.example that must configure it and remove the .example extension, docker-compose-updater.sh).
+2. Sequentially to update several but launching each one in the background.
+For this you must use the 2 sh that are inside the repository (apps-updater.sh.example that must configure it and remove the .example extension, docker-compose-updater.sh).
+3. If you only need to update a single application you should only use the docker-compose-updater.sh file
+
+For all options please read the documentation inside each sh file.
---
+++ docker-compose-updater.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#
+# Description
+# The sh file only works with an absolute path.
+# Additionally, the repository must have a docker-compose.yml file,
+# which will allow updating the application based on docker compose.
+# The repository must be public.
+#
+# How to use
+# Usage: {path}/docker-compose-updater.sh {github_username}/{github_repository}
+# Example: /var/docker-compose-updater.sh fabian/repository-example
+#
+
+# Get latest version
+get_latest_release() {
+    GITHUB_REPO="$1"
+    curl --silent "https://api.github.com/repos/$GITHUB_REPO/releases/latest" |
+        grep '"tag_name":' |
+        sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+# Download a new realease
+download_release_file() {
+    GITHUB_REPO="$1"
+    TMP_DIR="$2"
+    ZIP_FILE="$TMP_DIR/$3.zip"
+    curl -Ls --location --request GET https://api.github.com/repos/"$GITHUB_REPO"/releases/latest |
+        jq -r ".zipball_url" |
+        wget -qi - -O "$ZIP_FILE" &&
+        unzip -q -o "$ZIP_FILE" -d "$TMP_DIR" &&
+        cp -R "$TMP_DIR"/"$(unzip -Z -1 "$ZIP_FILE" | head -1)"/* "$TMP_DIR" &&
+        rm -R "${TMP_DIR:?}/$(unzip -Z -1 "$ZIP_FILE" | head -1)/" &&
+        rm "$ZIP_FILE"
+}
+
+# Function check exists directory
+checkExitsDirectory() {
+    directory="$1"
+    retval=""
+    if [[ -d "$directory" ]]; then
+        retval="true"
+    else
+        retval="false"
+    fi
+    echo $retval
+}
+
+GITHUB_REPOSITORY="$1"
+DEPLOYMENT_DIR="$1"
+TMP=$(cat "$DEPLOYMENT_DIR"/LATEST_VERSION.txt 2>/dev/null || true)
+CURRENT_VERSION="${TMP:-0.0.0}"
+LATEST_VERSION=$(get_latest_release "$GITHUB_REPOSITORY")
+
+# Check is a new version to deploy
+if [ "$CURRENT_VERSION" = "$LATEST_VERSION" ]; then
+    if [ -e "$DEPLOYMENT_DIR" ]; then
+        docker-compose -f "$DEPLOYMENT_DIR"/docker-compose.yml up -d
+    fi
+    echo "Docker compose is already running the latest version ($LATEST_VERSION)"
+else
+    # Check connection to internet
+    wget -q --spider http://google.com
+    check_internet=$?
+    if [ $check_internet -eq 0 ]; then
+
+        echo "Docker compose is running an old version (current: $CURRENT_VERSION, latest: $LATEST_VERSION)"
+
+        # Download the latest release's files
+        TMP_DIR=$(mktemp -d -t docker-compose-XXXXXXXXXX)
+        echo "$TMP_DIR"
+        download_release_file "$GITHUB_REPOSITORY" "$TMP_DIR" "$LATEST_VERSION"
+
+        # Copy previous deployment
+        is_exists_directory_repository=$(checkExitsDirectory "$DEPLOYMENT_DIR")
+        if [[ $is_exists_directory_repository == 'true' ]]; then
+            # cp -R "$DEPLOYMENT_DIR" "$(echo "$DEPLOYMENT_DIR" | rev | cut -d'/' -f2- | rev)/$(basename "$DEPLOYMENT_DIR")-old_deploy"
+            cp -R "$DEPLOYMENT_DIR" "$DEPLOYMENT_DIR-old_deploy"
+        fi
+
+        # Stop the current deployment
+        if [ -e "$DEPLOYMENT_DIR" ]; then
+            docker-compose -f "$DEPLOYMENT_DIR"/docker-compose.yml down
+        else
+            mkdir -p "$DEPLOYMENT_DIR"
+        fi
+
+        # Delete the current deployment
+        rm -rf "${DEPLOYMENT_DIR:?}/"*
+        mv "$TMP_DIR"/* "$DEPLOYMENT_DIR"
+
+        # Deploy the application
+        docker-compose -f "$DEPLOYMENT_DIR"/docker-compose.yml up -d
+
+        check_error_version_latest=$?
+        if [ $check_error_version_latest -eq 0 ]; then
+            ls -la TalentumLAB
+            echo "Successfully deploy latest"
+            # Store the latest version and remove temporary files
+            echo "$LATEST_VERSION" >"$DEPLOYMENT_DIR"/LATEST_VERSION.txt
+            echo "Docker compose is now running version ${LATEST_VERSION}"
+
+            # Remove copy temporal previus version
+            is_exists_previous_version=$(checkExitsDirectory "$DEPLOYMENT_DIR-old_deploy")
+            if [[ $is_exists_previous_version == 'true' ]]; then
+                name_folder_old="$DEPLOYMENT_DIR-old_deploy"
+                echo "$name_folder_old"
+                rm -rf "${name_folder_old:?}"
+            fi
+            ls -la TalentumLAB
+        else
+            # Delete the latest deployment with error
+            rm -rf "${DEPLOYMENT_DIR:?}/"*
+
+            # Rename previous version
+            mv "$DEPLOYMENT_DIR-old_deploy" "$DEPLOYMENT_DIR"
+
+            # Deploy previous version
+            docker-compose -f "$DEPLOYMENT_DIR"/docker-compose.yml down
+        fi
+    else
+        docker-compose -f "$DEPLOYMENT_DIR"/docker-compose.yml up -d
+    fi
+fi
